fa8ca182ac9b870712b9b1774967e9c9c67df7e2
Shubham Agarwal <shubhamagarwal@Shubhams-MacBook-Air.local>
2025-02-09 20:36:12 +0530

    n_estimator_22

--- src/model_building.py
+++ src/model_building.py
@@ -121,7 +121,7 @@ def main():
     try:
         # params = load_params('params.yaml')['model_building']
         params = {
-            'n_estimators': 9,
+            'n_estimators': 22,
             'random_state': 2
         }
         train_data = load_data('./data/processed/train_tfidf.csv')
