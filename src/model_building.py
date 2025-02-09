5493f1fc01bdafbf7dae83eb3e7a731b9d2b0891
Shubham Agarwal <shubhamagarwal@Shubhams-MacBook-Air.local>
2025-02-09 20:02:11 +0530

    manually updated n_estimator param and re-run pipeline

--- src/model_building.py
+++ src/model_building.py
@@ -121,7 +121,7 @@ def main():
     try:
         # params = load_params('params.yaml')['model_building']
         params = {
-            'n_estimators': 19,
+            'n_estimators': 25,
             'random_state': 2
         }
         train_data = load_data('./data/processed/train_tfidf.csv')
