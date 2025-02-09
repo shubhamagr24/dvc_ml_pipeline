d878e9f948eb8e58b73f17ae72bdf999106a9885
shubhamagr24 <shubhamagr24@gmil.com>
2025-02-09 23:28:34 +0530

    rectified dvc live metric tracking

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -123,9 +123,9 @@ def main():
 
         # Experiment tracking using dvclive
         with Live(save_dvc_exp=True) as live:
-            live.log_metric('accuracy', accuracy_score(y_test, y_test))
-            live.log_metric('precision', precision_score(y_test, y_test))
-            live.log_metric('recall', recall_score(y_test, y_test))
+            live.log_metric('accuracy', metrics['accuracy'])
+            live.log_metric('precision', metrics['precision'])
+            live.log_metric('recall', metrics['recall'])
 
             live.log_params(params)
         
